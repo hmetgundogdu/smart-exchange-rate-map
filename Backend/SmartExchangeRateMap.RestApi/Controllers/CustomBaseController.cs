c980ee2eb5227a4248f8d00aeed830aec013a165
zafer kırık <zafer.krk@hotmail.com>
2021-12-23 23:32:50 +0300

    Created simple structure

---
+++ Backend/SmartExchangeRateMap.RestApi/Controllers/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartExchangeRateMap.Application.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartExchangeRateMap.RestApi.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
---
+++ Backend/SmartExchangeRateMap.RestApi/Controllers/ExhangeController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartExchangeRateMap.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartExchangeRateMap.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExhangeController : CustomBaseController
+    {
+        private readonly IMediator _mediator;
+
+        public ExhangeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllWorldAsync([FromQuery]TestCommand testCommand)
+        {
+            var response = await _mediator.Send(testCommand);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
