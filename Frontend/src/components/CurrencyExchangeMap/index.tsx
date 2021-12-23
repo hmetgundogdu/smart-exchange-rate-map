a61f362970fd25b87f55a98d7b8e0ba3ba464608
Ahmet Gundogdu <furkan.gs.gs@gmail.com>
2021-12-23 23:15:40 +0300

    apply world map into map component

--- Frontend/src/components/CurrencyExchangeMap/index.tsx
+++ Frontend/src/components/CurrencyExchangeMap/index.tsx
@@ -1,10 +1,14 @@
+import { VectorMap } from '@south-paw/react-vector-maps';
+
+import world from '../../assets/world.json';
+
 interface CurrencyExchangeMapProps {
 
 }
 
 function CurrencyExchangeMap(props: CurrencyExchangeMapProps) {
     return (
-        <></>
+        <VectorMap {...world} />
     );
 }
 
