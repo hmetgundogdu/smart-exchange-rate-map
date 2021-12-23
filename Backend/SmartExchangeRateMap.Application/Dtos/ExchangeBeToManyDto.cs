3d8eae8087fb90bd12f606ef04e33f824f199139
zafer kırık <zafer.krk@hotmail.com>
2021-12-24 00:13:53 +0300

    Connection to exchange service will be provided

---
+++ Backend/SmartExchangeRateMap.Application/Dtos/ExchangeBeToManyDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartExchangeRateMap.Application.Dtos
+{
+    public class ExchangeBeToManyDto
+    {
+        public string BaseUnit { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public List<WorldUnits> Others { get; set; }
+        public class WorldUnits
+        {
+            public string CountryCode { get; set; }
+            public string CountryName { get; set; }
+            public double CalculatedPrice { get; set; }
+        }
+    }
+
+}
